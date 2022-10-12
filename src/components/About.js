9e32227356bbdad5e18dc5389874a24d6b50ac32
Cesar Hernandez <chema27281@gmail.com>
2022-10-12 15:25:10 -0400

    commit tree 87a6226afd2c7fc1fb72184bfd6a3ff2e94b9419

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,18 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">About
+      <img src={image} alt='I made this' />
+      <h2>About Me</h2>
+      <p> </p>
+
+
+    </div>
+
+
+  )
+
 }
 
 export default About;
